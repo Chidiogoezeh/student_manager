5b6b9f261596575e4c3d511441932d21133d8b91
Chidiogo <chidiogoezeh.me@gmail.com>
2026-08-21 12:54:45 +0100

    Use tuple struct for student ID

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+struct StudentId(u32);
 struct Student {
-    id: u32,
+    id: StudentId,
     name: String,
     age: u8,
     course: String,
@@ -7,7 +8,7 @@ struct Student {
 }
 fn main() {
     let mut student = Student {
-        id: 1,
+        id: StudentId(1),
         name: String::from("Chidiogo"),
         age: 30,
         course: String::from("Backend Development"),
@@ -15,26 +16,10 @@ fn main() {
     };
 
     student.score = 90.4;
-    println!("Student: {}", student.score);
 
-    let age = student.age;
-    println!("Age: {}", age);
-
-    let age = age + 1;
-    println!("New age: {}", age);
-
-    let student_id = "1004";
-    let student_id = student_id.parse::<u32>().unwrap();
-
-    println!("Passed Student ID (u32): {}", student_id);
-
-    // Mutability
-    let mut score = 75;
-    score = 86;
-    println!("Mutable score: {}", score);
-
-    // Shadowing
-    let score = 75;
-    let score = 86;
-    println!("Shadowed score: {}", score);
+    println!("Student ID: {}", student.id.0);
+    println!("Student Name: {}", student.name);
+    println!("Student Age: {}", student.age);
+    println!("Student Course: {}", student.course);
+    println!("Student Score: {}", student.score);
 }
