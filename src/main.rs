12acb6bce21339b8169aa903edc640d08c187f28
Chidiogo <chidiogoezeh.me@gmail.com>
2026-08-21 17:26:55 +0100

    Adding menu option using enum

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,13 @@ struct Student {
     course: String,
     score: f64,
 }
+
+enum MenuOption {
+    Add,
+    Delete,
+    List,
+    Exit,
+}
 fn main() {
-    let mut student = Student {
-        id: StudentId(1),
-        name: String::from("Chidiogo"),
-        age: 30,
-        course: String::from("Backend Development"),
-        score: 85.6,
-    };
-
-    student.score = 90.4;
-
-    println!("Student ID: {}", student.id.0);
-    println!("Student Name: {}", student.name);
-    println!("Student Age: {}", student.age);
-    println!("Student Course: {}", student.course);
-    println!("Student Score: {}", student.score);
+    println!("Student Management System");
 }
